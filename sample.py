e0c469b63b429338e6fc6b35288e452e832a5cd0
Josh Kasuboski <josh.kasuboski@gmail.com>
2021-03-27 14:32:37 -0500

    use multiple gpus started with a process for each

--- sample.py
+++ sample.py
@@ -6,13 +6,6 @@ from PIL import Image
 import concurrent.futures
 import time
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-torch.cuda.empty_cache()
-
-# Model checkpoints
-srgan_checkpoint = "./checkpoint_srgan.pth.tar"
-srgan_generator = torch.load(srgan_checkpoint)['generator'].to(device)
-srgan_generator.eval()
 
 def visualize_sr(img, halve=False):
     """
@@ -37,31 +30,50 @@ def visualize_sr(img, halve=False):
                            Image.BICUBIC)
 
     # Super-resolution (SR) with SRGAN
-    sr_img_srgan = srgan_generator(convert_image(lr_img, source='pil', target='imagenet-norm').unsqueeze(0).to(device))
+    sr_img_srgan = srgan_generator(convert_image(
+        lr_img, source='pil', target='imagenet-norm').unsqueeze(0).to(device))
     sr_img_srgan = sr_img_srgan.squeeze(0).cpu().detach()
     sr_img_srgan = convert_image(sr_img_srgan, source='[-1, 1]', target='pil')
 
     return sr_img_srgan
+
 
 def up_file(inFile, outFile):
     if os.path.exists(outFile) == False:
         img = visualize_sr(inFile)
         img.save(outFile)
 
+
 if __name__ == "__main__":
     start_time = time.process_time()
+    print("Beginning folder walk")
     in_folder = "../in-frames"
     out_folder = "../out-frames"
     (_, _, filenames) = next(os.walk(in_folder))
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    n = 4
+    all_files = [filenames[i::n] for i in range(n)]
+
+    i = sys.argv[1]
+    device = torch.device('cuda:{}'.format(i))
+    torch.cuda.empty_cache()
+
+    # Model checkpoints
+    srgan_checkpoint = "./checkpoint_srgan.pth.tar"
+    srgan_generator = torch.load(srgan_checkpoint)['generator'].to(device)
+    srgan_generator.eval()
+
+    # first gpu has some extra allocation :shrug: and gpu 1 oom
+    workers = 4
+    workers = 3 if int(i) == 0 else workers
+    with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
         futures = []
-        for f in filenames:
+        for f in all_files[int(i)]:
             in_file = os.path.join(in_folder, f)
             out_file = os.path.join(out_folder, f)
             futures.append(executor.submit(up_file, in_file, out_file))
         for future in concurrent.futures.as_completed(futures):
             future.result()
-                
+
         print(time.process_time() - start_time)
     torch.cuda.empty_cache()
---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python sample.py 0 &
+python sample.py 1 &
+python sample.py 2 &
+python sample.py 3 &
+
+wait
